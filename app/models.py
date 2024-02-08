318c3c80b9cdfbf529ec982ee30fa0d1d954dc7a
Nicholas L <NLoo1@outlook.com>
2024-02-08 15:02:55 -0500

    Added cupcake model, cupcake list, get cupcake by id, add cupcake

--- app/models.py
+++ app/models.py
@@ -11,8 +11,17 @@ class Cupcake(db.Model):
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     flavor = db.Column(db.String(50), nullable=False)
     size = db.Column(db.String(50), nullable=False)
-    rating = db.Column(db.Float, nullable=False, default='')
-    image = db.Column(db.Text, nullable=False, default='')
+    rating = db.Column(db.Float, nullable=False)
+    image = db.Column(db.Text, nullable=False, default='https://tinyurl.com/demo-cupcake')
+
+    def serialize(self):
+        return{
+            "id": self.id,
+            "flavor": self.flavor,
+            "size": self.flavor,
+            "rating": self.rating,
+            "image": self.image
+        }
 
     @classmethod
     def __repr__(self):
